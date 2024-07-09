875bfad9c36d60d971a685c37a80e8c735efae3b
Shegzboyer12 <segunjoshua85@gmail.com>
2024-07-09 15:48:03 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import PlayersList from "./playersList";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-center my-4">Players List</h1>
+      <PlayersList />
+    </div>
+  );
+};
+
+export default App;
---
+++ players.js
@@ -0,0 +1,36 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami",
+    nationality: "Argentinian",
+    jerseyNumber: 10,
+    age: 36,
+    imageUrl: "https://example.com/messi.jpg",
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al Nassr",
+    nationality: "Portuguese",
+    jerseyNumber: 7,
+    age: 38,
+    imageUrl: "https://example.com/ronaldo.jpg",
+  },
+  {
+    name: "Neymar Jr.",
+    team: "Al Hilal",
+    nationality: "Brazilian",
+    jerseyNumber: 10,
+    age: 32,
+    imageUrl: "https://example.com/neymar.jpg",
+  },
+  {
+    name: "Kylian Mbappe",
+    team: "Paris Saint-Germain",
+    nationality: "French",
+    jerseyNumber: 7,
+    age: 25,
+    imageUrl: "https://example.com/mbappe.jpg",
+  },
+];
+
+export default players;
